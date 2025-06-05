779208962fc840150516737b33cf8a3dd014d450
Michael Lei <michaelleie0ca@gmail.com>
2025-06-04 22:40:03 -0700

    Add github and google provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,13 +2,19 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
 import { anonymous } from "better-auth/plugins";
+import { env } from "@/env";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
-  emailAndPassword: {
-    enabled: true,
+  github: {
+    clientId: env.GITHUB_CLIENT_ID,
+    clientSecret: env.GITHUB_CLIENT_SECRET,
+  },
+  google: {
+    clientId: env.GOOGLE_CLIENT_ID,
+    clientSecret: env.GOOGLE_CLIENT_SECRET,
   },
   plugins: [anonymous()],
 });
