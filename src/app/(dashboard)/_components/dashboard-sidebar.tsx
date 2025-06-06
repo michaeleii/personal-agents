8408eb8ff0ab42379aca5faf3a0e31258cabe40f
Michael Lei <michaelleie0ca@gmail.com>
2025-06-05 20:29:50 -0700

    Add drawer for mobile view in user button

--- src/app/(dashboard)/_components/dashboard-sidebar.tsx
+++ src/app/(dashboard)/_components/dashboard-sidebar.tsx
@@ -1,6 +1,16 @@
 "use client";
 import GeneratedAvatar from "@/components/generated-avatar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -21,6 +31,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { deleteUser, signOut } from "@/lib/auth-client";
 import { cn } from "@/lib/utils";
 import type { User } from "better-auth";
@@ -150,6 +161,81 @@ export default function DashboardSidebar({
 
 function DashboardUserButton({ user }: { user: UserWithAnonymous }) {
   const router = useRouter();
+  const isMobile = useIsMobile();
+
+  async function onLogout() {
+    if (user.isAnonymous) {
+      await deleteUser({
+        callbackURL: "/sign-in",
+        fetchOptions: {
+          onSuccess: () => {
+            router.refresh();
+          },
+        },
+      });
+    }
+    await signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.refresh();
+        },
+      },
+    });
+  }
+
+  if (isMobile) {
+    return (
+      <Drawer>
+        <DrawerTrigger className="focus-visible:border/10 flex w-full items-center justify-between gap-x-2 overflow-hidden rounded-lg p-3 transition-all hover:bg-white/10">
+          {user.image ? (
+            <Avatar>
+              <AvatarImage src={user.image} />
+            </Avatar>
+          ) : (
+            <GeneratedAvatar
+              seed={user.name}
+              variant="initials"
+              className="mr-2 size-9"
+            />
+          )}
+          <div className="flex min-w-0 flex-1 flex-col gap-0.5 overflow-hidden text-left">
+            <p className="truncate text-sm">{user.name}</p>
+            {!user.isAnonymous && (
+              <p className="truncate text-xs">{user.email}</p>
+            )}
+          </div>
+          <ChevronDownIcon className="size-4 shrink-0" />
+        </DrawerTrigger>
+        <DrawerContent className="border-border/10 text-sidebar-foreground bg-[#132621]">
+          <DrawerHeader>
+            <div className="flex flex-col gap-1">
+              <DrawerTitle className="text-sidebar-muted-foreground truncate font-medium">
+                {user.name}
+              </DrawerTitle>
+              {!user.isAnonymous && (
+                <DrawerDescription className="truncate text-sm font-normal">
+                  {user.email}
+                </DrawerDescription>
+              )}
+            </div>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button className="border-border/10 text-sidebar-muted-foreground hover:text-sidebar-accent-foreground border bg-white/5 hover:bg-white/10">
+              <CreditCardIcon className="text-sidebar-muted-foreground size-4" />
+              Billing
+            </Button>
+            <Button
+              className="border-border/10 text-sidebar-muted-foreground hover:text-sidebar-accent-foreground border bg-white/5 hover:bg-white/10"
+              onClick={onLogout}
+            >
+              <LogOutIcon className="text-sidebar-muted-foreground size-4" />
+              Log Out
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus-visible:border/10 flex w-full items-center justify-between gap-x-2 overflow-hidden rounded-lg p-3 transition-all hover:bg-white/10">
@@ -193,25 +279,7 @@ function DashboardUserButton({ user }: { user: UserWithAnonymous }) {
         </DropdownMenuItem>
         <DropdownMenuItem
           className="text-sidebar-muted-foreground focus:text-sidebar-accent-foreground focus:bg-white/10"
-          onClick={async () => {
-            if (user.isAnonymous) {
-              await deleteUser({
-                callbackURL: "/sign-in",
-                fetchOptions: {
-                  onSuccess: () => {
-                    router.refresh();
-                  },
-                },
-              });
-            }
-            await signOut({
-              fetchOptions: {
-                onSuccess: () => {
-                  router.refresh();
-                },
-              },
-            });
-          }}
+          onClick={onLogout}
         >
           <LogOutIcon className="text-sidebar-muted-foreground size-4" />
           Log Out
