42dbeebaf745e13284e294ddcd166d12df0e5596
Michael Lei <michaelleie0ca@gmail.com>
2025-06-17 16:50:03 -0700

    fix: refactor database initialization to use neon client

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,7 @@
 import { env } from "@/env";
+import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "@/db/schema";
 
-export const db = drizzle(env.DATABASE_URL, {
-  schema,
-  casing: "snake_case",
-});
+const sql = neon(env.DATABASE_URL);
+export const db = drizzle({ client: sql, schema, casing: "snake_case" });
