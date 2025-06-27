9beab8f998325aa4616726b4f53fc0bb58e85f7c
Michael Lei <michaelleie0ca@gmail.com>
2025-06-27 00:40:52 -0700

    refactor: remove mutation cache and related logic from query client

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -1,25 +1,10 @@
 import {
   defaultShouldDehydrateQuery,
-  matchQuery,
-  MutationCache,
   QueryClient,
-  type QueryKey,
 } from "@tanstack/react-query";
 // import superjson from "superjson";
 export function makeQueryClient() {
-  const queryClient = new QueryClient({
-    mutationCache: new MutationCache({
-      onSuccess: (_data, _variables, _context, mutation) => {
-        queryClient.invalidateQueries({
-          predicate: (query) =>
-            // invalidate all matching tags at once
-            // or everything if no meta is provided
-            mutation.meta?.invalidateQueries?.some((queryKey) =>
-              matchQuery({ queryKey }, query)
-            ) ?? true,
-        });
-      },
-    }),
+  return new QueryClient({
     defaultOptions: {
       queries: {
         staleTime: 30 * 1000,
@@ -35,13 +20,4 @@ export function makeQueryClient() {
       },
     },
   });
-  return queryClient;
-}
-
-declare module "@tanstack/react-query" {
-  interface Register {
-    mutationMeta: {
-      invalidateQueries?: QueryKey[];
-    };
-  }
 }
