19181e3307808f61e75ec61c7a39a4a5925f84dc
Michael Lei <michaelleie0ca@gmail.com>
2025-06-27 00:30:08 -0700

    enhance query client with mutation cache and query invalidation logic

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -1,10 +1,25 @@
 import {
   defaultShouldDehydrateQuery,
+  matchQuery,
+  MutationCache,
   QueryClient,
+  type QueryKey,
 } from "@tanstack/react-query";
 // import superjson from "superjson";
 export function makeQueryClient() {
-  return new QueryClient({
+  const queryClient = new QueryClient({
+    mutationCache: new MutationCache({
+      onSuccess: (_data, _variables, _context, mutation) => {
+        queryClient.invalidateQueries({
+          predicate: (query) =>
+            // invalidate all matching tags at once
+            // or everything if no meta is provided
+            mutation.meta?.invalidateQueries?.some((queryKey) =>
+              matchQuery({ queryKey }, query)
+            ) ?? true,
+        });
+      },
+    }),
     defaultOptions: {
       queries: {
         staleTime: 30 * 1000,
@@ -20,4 +35,13 @@ export function makeQueryClient() {
       },
     },
   });
+  return queryClient;
+}
+
+declare module "@tanstack/react-query" {
+  interface Register {
+    mutationMeta: {
+      invalidateQueries?: QueryKey[];
+    };
+  }
 }
